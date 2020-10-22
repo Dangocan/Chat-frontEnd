44510740e6bd6606b499222429e7543305798ec0
Dangocan <danilogc1999@gmail.com>
2020-10-21 21:53:55 -0300

    .

---
+++ precache-manifest.8b41d9c9579afbc9b74d60fbf903a883.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cf299189dcb14e47b7d96d1f7e2d905",
+    "url": "/index.html"
+  },
+  {
+    "revision": "85fe31de88fc09eafc25",
+    "url": "/static/js/2.0edd4320.chunk.js"
+  },
+  {
+    "revision": "f65b45f562155f0fa2653b346e3552a6",
+    "url": "/static/js/2.0edd4320.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "223243037c1c2e0751ce",
+    "url": "/static/js/main.95707509.chunk.js"
+  },
+  {
+    "revision": "02431dfc46b843c90051",
+    "url": "/static/js/runtime-main.3a6df2d0.js"
+  }
+]);
